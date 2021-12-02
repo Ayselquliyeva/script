cce9a17953297176b945c5eddd41bc7e4d1727b0
Ayselquliyeva <92804962+Ayselquliyeva@users.noreply.github.com>
2021-12-02 20:03:54 +0400

    Add files via upload

---
+++ 4/script.js
@@ -0,0 +1,11 @@
+    function show(){
+        let inpt=document.getElementById("bir").value;
+        let ekran="";
+
+        for(i=0;i<inpt.value;i++){
+            ekran+=`<input/>`
+        }
+    }
+    inpt.innerHtm=inpt;
+
+        
